7a9d08c783b2f82fce33d7282334ff3567970cad
MehmetCan-23 <mehmetcancavusoglu@hotmail.com>
2025-02-21 21:40:13 +0300

    tests completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import org.example.mobile.Contact;
+import org.example.mobile.MobilePhone;
+import org.example.models.Grocery;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        //Grocery grocery = new Grocery();
+        //grocery.startGrocery();
+
+        System.out.println("****************************************************");
+
+        MobilePhone mobilePhone = new MobilePhone("23456789", new ArrayList<>());
+        mobilePhone.addNewContact(new Contact("Bob", "31415926"));
+        mobilePhone.addNewContact(new Contact("Alice", "16180339"));
+        mobilePhone.addNewContact(new Contact("Tom", "11235813"));
+        mobilePhone.addNewContact(new Contact("Jane", "23571113"));
+        mobilePhone.printContacts();
 
     }
 }
